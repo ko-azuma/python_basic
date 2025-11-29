bc7587431b1597e3f55c75e904d61d0117f8b93d
k_azuma <k.azuma@avanzare-tech.jp>
2025-11-29 19:18:41 +0900

    python基礎研修⑧終了

---
+++ python_basic_62_drink.py
@@ -0,0 +1,12 @@
+from python_basic_62_menu_item import MenuItem
+
+class Drink(MenuItem):
+    # __init__ メソッドを定義してください
+    # 引数にname, price, amountを受け取れるようにしてください。
+    # super()を用いて、親クラスの__init__メソッドを呼び出してください。
+    # __init__メソッドの中で、self.amountに引数amountの値を代入してください。
+    def __init__(self, name, price,amount):
+        super().__init__(name, price)
+        self.amount = amount
+    def info(self):
+        return self.name + ': ¥' + str(self.price) + ' (' + str(self.amount) + 'mL)'
